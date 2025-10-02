fd891ef71f9f39f4c9df167c95e78809f468518d
mavr1iq <kivar2006@gmail.com>
2025-10-02 14:42:11 +0300

    Initial commit

---
+++ src/test/java/org/java/web/intergalacticmarketplace/IntergalacticMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java.web.intergalacticmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntergalacticMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
